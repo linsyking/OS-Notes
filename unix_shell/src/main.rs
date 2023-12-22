84344d703cffc825191e8c7ee1d8cd8df71a3a31
linsyking <kxiang@umich.edu>
2023-12-22 14:38:04 -0500

    feat: add lexer

--- unix_shell/src/main.rs
+++ unix_shell/src/main.rs
@@ -15,7 +15,78 @@ enum Interrupt {
     Exit(i32),
 }
 
-fn execute(line: String) -> Result<(), Interrupt> {
+#[derive(Debug)]
+enum Token {
+    Str(String),
+    Pipe,
+    RightRedirect,
+    LeftRedirect,
+}
+
+fn lex(line: &String) -> Vec<Token> {
+    // A simple lexer
+    let mut toks = Vec::new();
+    let mut cur = String::new();
+    let mut is_in_str = false;
+    let mut it = line.chars();
+    while let Some(c) = it.next() {
+        match c {
+            '\\' => {
+                let cn = it.next().unwrap();
+                cur.push(c);
+                cur.push(cn);
+                continue;
+            }
+            '"' => {
+                if is_in_str {
+                    is_in_str = false;
+                    // Terminate string
+                    toks.push(Token::Str(cur.clone()));
+                    cur.clear();
+                } else {
+                    is_in_str = true;
+                }
+            }
+            _ if is_in_str => {
+                cur.push(c);
+            }
+            ' ' => {
+                toks.push(Token::Str(cur.clone()));
+                cur.clear();
+            }
+            '|' => {
+                // Pipe
+                toks.push(Token::Str(cur.clone()));
+                toks.push(Token::Pipe);
+                cur.clear();
+            }
+            '>' => {
+                // R-Redirect
+                toks.push(Token::Str(cur.clone()));
+                toks.push(Token::RightRedirect);
+                cur.clear();
+            }
+            '<' => {
+                // L-Redirect
+                toks.push(Token::Str(cur.clone()));
+                toks.push(Token::LeftRedirect);
+                cur.clear();
+            }
+            '\0' => {
+                // EOF
+                toks.push(Token::Str(cur.clone()));
+                cur.clear();
+                break;
+            }
+            _ => {
+                cur.push(c);
+            }
+        }
+    }
+    toks
+}
+
+fn execute(line: &String) -> Result<(), Interrupt> {
     let args: Vec<_> = line.trim().split(" ").collect();
     let len = args.len();
     if len == 0 {
@@ -61,7 +132,7 @@ fn main() {
                 // EOF
                 return;
             } else {
-                if let Err(e) = execute(line.clone()) {
+                if let Err(e) = execute(&line) {
                     match e {
                         Interrupt::SyntaxError => {
                             eprintln!("Syntax Error!");
