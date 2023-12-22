7ef92c99444c4ea7a92f67d8fec7eceed9ac9267
linsyking <kxiang@umich.edu>
2023-12-22 13:43:35 -0500

    feat: basic fork

---
+++ unix_shell/Readme.md
@@ -0,0 +1,9 @@
+# Unix Shell
+
+(Project described in *Operating System Concepts, 10th edition*, pp. 204)
+
+---
+
+Writing this in C needs using `fork()`, `exec()`, `wait()`, `dup2()`, and `pipe()`.
+
+In Rust we use `nix` library to call those syscalls.
--- unix_shell/src/main.rs
+++ unix_shell/src/main.rs
@@ -1,3 +1,87 @@
+use nix::errno::Errno;
+use nix::sys::wait::wait;
+use nix::unistd::ForkResult;
+use nix::unistd::{dup2, execvp, fork};
+use std::ffi::{CStr, CString};
+use std::{
+    io::{self, BufRead, Write},
+    process::exit,
+};
+#[derive(Debug)]
+enum Interrupt {
+    SyntaxError,
+    ExecError(Errno),
+    ForkError,
+    Exit(i32),
+}
+
+fn execute(line: String) -> Result<(), Interrupt> {
+    let args: Vec<_> = line.trim().split(" ").collect();
+    let len = args.len();
+    if len == 0 {
+        return Ok(());
+    }
+    match args[0] {
+        ";;" | "exit" if len == 1 => Err(Interrupt::Exit(0)),
+        _ => {
+            // Creating the child process
+            let pres = unsafe { fork() }.map_err(|_| Interrupt::ForkError)?;
+            match pres {
+                ForkResult::Parent { child } => {
+                    println!(
+                        "Parent process, waiting for the child (pid: {}) to complete...",
+                        child.as_raw()
+                    );
+                    wait().map_err(|e| Interrupt::ExecError(e))?;
+                    println!("Child process {} exited!", child.as_raw());
+                }
+                ForkResult::Child => {
+                    let pname = CString::new(args[0]).unwrap();
+                    let pname = pname.as_c_str();
+                    let pargs = args.clone();
+                    let pargs: Vec<CString> =
+                        pargs.iter().map(|x| CString::new(*x).unwrap()).collect();
+                    let pargs: Vec<&CStr> = pargs.iter().map(|x| x.as_c_str()).collect();
+                    execvp(pname, &pargs).map_err(|e| Interrupt::ExecError(e))?;
+                }
+            }
+            Ok(())
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    loop {
+        let mut line = String::new();
+        print!("sh> ");
+        io::stdout().flush().unwrap();
+        if let Ok(len) = stdin.lock().read_line(&mut line) {
+            if len == 0 {
+                // EOF
+                return;
+            } else {
+                if let Err(e) = execute(line.clone()) {
+                    match e {
+                        Interrupt::SyntaxError => {
+                            eprintln!("Syntax Error!");
+                            exit(1);
+                        }
+                        Interrupt::ForkError => {
+                            eprintln!("Fork Error!");
+                            exit(1);
+                        }
+                        Interrupt::ExecError(e) => {
+                            eprintln!("Exec Error: {}", e.desc());
+                            exit(1);
+                        }
+                        Interrupt::Exit(code) => exit(code),
+                    }
+                }
+            }
+        } else {
+            // Error!
+            exit(1);
+        }
+    }
 }
