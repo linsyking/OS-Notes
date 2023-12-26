7fa0732ceb7956e4c40ad9aa9e7a4dad5a8c688e
linsyking <kxiang@umich.edu>
2023-12-25 20:26:32 -0500

    feat: add tests

---
+++ unix_shell/tests/syntax.rs
@@ -0,0 +1,26 @@
+use unix_shell::ast::parse;
+use unix_shell::eval::check_prog;
+use unix_shell::lex::lex;
+
+fn run_test(s: &str, pass: bool) {
+    let line = String::from(s);
+    let l = lex(&line);
+    let ast = parse(l).unwrap();
+    if pass {
+        check_prog(&ast).unwrap();
+    } else {
+        check_prog(&ast).unwrap_err();
+    }
+}
+
+// #[cfg(test)]
+#[test]
+fn syntax() {
+    run_test("ls | ls", true);
+    run_test("ls > a > a", false);
+    run_test("ls > a < a", false);
+    run_test("ls > a | cat", false);
+    run_test("ls < a < s", false);
+    run_test("ls | cat > b | m", false);
+    run_test("ls | cat < a", false);
+}
